6ced2bd7efa1df933f4dfde89e3525add1e1fa23
TosinJs <tosinjsamuel@gmail.com>
2024-05-22 16:02:47 +0100

    Update

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,7 +1,13 @@
-import connect from '@gandalf-network/connect/dist/connect';
+import Connect from '@gandalf-network/connect';
 import Eye, { Source } from '../eyeofsauron';
 import { Request, Response, Router } from 'express';
 const router: Router = Router();
+
+const connect = new Connect({
+    publicKey: "Your Public Key", 
+    redirectURL: "Your Redirect URL",
+    services: { "YOUTUBE": true }
+})
 
 // returns the url to connect our app to youtube using gandalf
 router.get("/connect", async (req: Request, res: Response) => {
@@ -15,7 +21,7 @@ router.get("/connect", async (req: Request, res: Response) => {
 // To fetch users history
 router.get("/youtube", async (req: Request, res: Response) => {
     const currentUrl = req.query.href; // Gets the current browser URL from frontend affect connecting
-    const dataKey = connect.getDataKeyFromURL(String(currentUrl));
+    const dataKey = Connect.getDataKeyFromURL(String(currentUrl));
 
     const eye = new Eye({ privateKey: "0x92b44ab814fb4e6a3fb13754e5a98b329a51c3e11c300d976a970305e5d496b4" })
 
